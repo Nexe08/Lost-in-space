95b336bead1e6239680a5f97a38503ff950dbed2
nexe <87233162+Nexe08@users.noreply.github.com>
2021-12-19 12:19:24 +0530

    Feature (#2)
    * Removed 'Hard thuster'
    
    * Added ship durability
    
    * Added Early HUD Prototype
    
    * Added templete for powerUp.

--- Script/Ship.gd
+++ Script/Ship.gd
@@ -4,6 +4,7 @@ extends RigidBody2D
 # tweek mass facter for modify ship control
 
 export (Vector2) var MAX_VELOCITY = Vector2(200, 300)
+export (int) var durability = 3
 
 var thrust = Vector2(0, -1000)
 var touque = 10000
@@ -20,18 +21,20 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
+    $CanvasLayer/Label.text = String(durability)
     $CanvasLayer/Label2.text = String(linear_velocity)
     $CanvasLayer/Label3.text = String(angular_velocity)
     Global.constrain_in_screen(self, Vector2(16, 16))
     
     # clamping 
+    durability = clamp(durability, 0, 3) # hard codded max durability
     
-    angular_velocity = clamp(angular_velocity, -5, 5)
-    
-    if linear_velocity.x > MAX_VELOCITY.x:
-        linear_velocity.x = lerp(linear_velocity.x, MAX_VELOCITY.x, 10 * delta)
-    elif linear_velocity.x < -MAX_VELOCITY.x:
-        linear_velocity.x = lerp(linear_velocity.x, -MAX_VELOCITY.x, 10 * delta)
+#    angular_velocity = clamp(angular_velocity, -5, 5)
+#
+#    if linear_velocity.x > MAX_VELOCITY.x:
+#        linear_velocity.x = lerp(linear_velocity.x, MAX_VELOCITY.x, 10 * delta)
+#    elif linear_velocity.x < -MAX_VELOCITY.x:
+#        linear_velocity.x = lerp(linear_velocity.x, -MAX_VELOCITY.x, 10 * delta)
     
     if linear_velocity.y < -MAX_VELOCITY.y:
         linear_velocity.y = lerp(linear_velocity.y, -MAX_VELOCITY.y, 10 * delta)
@@ -67,11 +70,6 @@ func _integrate_forces(_state: Physics2DDirectBodyState) -> void:
     applied_torque = rotation_dir * touque
 
 
-func _unhandled_input(event: InputEvent) -> void:
-    if event.is_action_pressed("space"):
-        apply_impulse(Vector2.ZERO, Vector2(0, -900).rotated(rotation))
-
-
 # Barrier for ship to down side
 func _on_PrevPositionAssigner_timeout() -> void:
     if linear_velocity.y < 0: # moving up
@@ -84,5 +82,9 @@ func _on_PrevPositionAssigner_timeout() -> void:
 
 # collision with astroide
 func _on_Ship_body_entered(_body: Node) -> void:
-    pass
-#    queue_free()
+    if durability <= 0:
+        queue_free()
+        return
+    
+    durability -= 1
+    # add effects
